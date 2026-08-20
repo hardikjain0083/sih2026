a7bc1c28c7b3e17130584c82253987ab561a3738
Atharv Porwal <176670425+porwalbuildit@users.noreply.github.com>
2026-08-21 02:12:32 +0530

    Connect frontend to deployed backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://suraksha-maps-api-2026.onrender.com',
 });
 
 api.interceptors.response.use(
